08bb0862fb186cc35861cb19feca67c9ff78e1d7
mel <manonuev@my.centennialcollege.ca>
2023-04-14 17:14:44 -0400

    initial commit

---
+++ Quiz4_COMP367/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Quiz4_COMP367.Models;
+
+namespace Quiz4_COMP367.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewData["Message"] = "Database access has already been done on April 14, 2023 by Mel Vincent Anonuevo";
+        return View("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Quiz4_COMP367/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Quiz4_COMP367.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
